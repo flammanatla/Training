aad358e18134be2f9a689aac83527aa79f94e823
Nataliia.Moroz <nataliia.moroz@adstream.com>
2016-05-22 20:23:52 +0100

    calculating distance between two points

---
+++ sandbox/src/main/java/uk/stqa/sandbox/Point.java
@@ -0,0 +1,16 @@
+package uk.stqa.sandbox;
+
+/**
+ * Created by natla on 22/05/2016.
+ */
+
+public class Point {
+
+  public double x;
+  public double y;
+
+  public Point(double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+}
--- sandbox/src/main/java/uk/stqa/sandbox/Training.java
+++ sandbox/src/main/java/uk/stqa/sandbox/Training.java
@@ -4,6 +4,15 @@ public class Training {
 	
 	public static void main(String[] args){
 		System.out.println("Hello, world!!(one more time)");
+
+		// task #2
+    Point p1 = new Point(1, 1);
+		Point p2 = new Point(5, 5);
+		System.out.println("Distance =" + distance(p1, p2));
 	}
-	
+
+  public static double distance(Point p1, Point p2){
+    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+  }
+
 }
