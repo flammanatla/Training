21e31c242eb96a16526d3631c9250f9861b7547c
Nataliia.Moroz <nataliia.moroz@adstream.com>
2016-07-31 16:53:08 +0100

    no message

--- addressbook-web-tests/src/test/java/uk/stqa/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/uk/stqa/addressbook/appmanager/ApplicationManager.java
@@ -51,7 +51,7 @@ public class ApplicationManager {
         } else {
           DesiredCapabilities capabilities = new DesiredCapabilities();
           capabilities.setBrowserName(browser);
-          capabilities.setPlatform(Platform.fromString(properties.getProperty("platform", "Mac")));
+          capabilities.setPlatform(Platform.fromString(properties.getProperty("platform")));
           wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
 
